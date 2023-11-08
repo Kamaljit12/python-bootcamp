1983b7b82c92054046aa53124aa362c48b945fbd
Kamal Jit singh <89628021+Kamaljit12@users.noreply.github.com>
2023-11-08 07:25:35 +0000

    here i have created some fucntion

--- bascis/basic_functions.py
+++ bascis/basic_functions.py
@@ -25,3 +25,68 @@ def is_even(n):
 
 is_even(10)
 is_even(7)
+
+# string call
+def full_name(fname, lname):
+    print(fname, lname)
+
+
+full_name('kamal', 'singh')
+
+# square functions
+
+def square(n):
+    res = n**2
+    print("square is :", res)
+
+square(10)
+
+# square root finder
+
+def square_root(n):
+    res = n**0.5
+    
+    print('square root of {} is '.format(n), res)
+
+square_root(8)
+square_root(9)
+
+
+# get square of the list
+ls = [1, 2, 3, 4, 5]
+
+def get_square(n):
+    for i in n:
+        res = i**2
+        print('square of the list is :', res)
+
+get_square(ls)
+
+
+# get the square of the list
+def square_of_list(lst):
+    for i in lst:
+        ls = i**2
+
+print("square_of_list is =", ls)
+square_of_list(ls)
+
+
+# squre fucntion with default value
+
+def square_fun(n=1):
+    res = n*n
+    print("squre of {} is :".format(n), res)
+
+square_fun(10)
+square_fun(5)
+# i will not pass any argumnet even though i will get result
+square_fun()
+
+
+# fuction argument with default value
+def add_num(a = 5, b = 10):
+    res = a+b
+    print("sum of {} and {} is :".format(a, b), res)
+
+add_num()
