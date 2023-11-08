4d8e38b4bf2a70756a0823441cd420291bab37fb
Kamal Jit singh <89628021+Kamaljit12@users.noreply.github.com>
2023-11-08 08:28:07 +0000

    this is for the data types in details

---
+++ bascis/data_types.py
@@ -0,0 +1,75 @@
+# boolean data
+a  = True
+b = False
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+
+# string
+
+name = "kamal jit singh"
+print(name)
+# data type of the variables
+print(type(name))
+
+fname = "kamal"
+lname = "singh"
+print(fname,lname)
+# data type of the variables
+type(fname)
+type(lname)
+
+greet = 'hello'
+print(greet)
+# data type of the variables
+print(type(greet))
+
+
+num_string = "100"
+num_string_1 = "1.02"
+print(num_string)
+print(num_string_1)
+# data type of the variables
+print(type(num_string))
+print(type(num_string_1))
+
+
+
+# numerical values
+
+# integer data
+int_num = 100
+print(int_num)
+print(type(int_num))
+
+# float data
+
+float_num = 10.254
+print(float_num)
+print(type(float_num))
+
+num_ls = [1, 3, 4]
+print(num_ls)
+print(type(num_ls))
+
+name_ls = ["kamal", "chanda", 'durga', 'somil']
+print(name_ls)
+print(type(name_ls))
+
+mix_ls = ["kamal", 1000, 24.5, True, False]
+print(mix_ls)
+print(type(mix_ls))
+
+# tuples
+
+tup = ("kamal", 100, 10.5)
+print(tup)
+print(type(tup))
+
+# dictionary
+
+dic = {"name": "kamal", "age": 24, "salary": None}
+print(dic)
+print(type(dic))
---
+++ bascis/list.py
@@ -0,0 +1,33 @@
+# simple string list
+ls = ["kamal", 'chanda', 'durga', 'somil', 'sona', 'ramasankar']
+print(ls)
+
+# list of multiple data types
+ls1 = ['kamal', 152, '100', 10.5]
+print(ls1)
+
+
+# list of list
+
+ls2 = [[1, 2], [3, 4], [4, 5]]
+print(ls2)
+
+# list of tupe and dict
+
+ls3 = [[1, 2, 3], (4, 5, 6), 7, 8, 9]
+print(ls3)
+
+# crete list of tupel
+
+tup = (1, 2, 3, 4, 5)
+print(tup)
+# convert tuple into list
+ls4 = list(tup)
+
+print(ls4)
+
+
+# dict in list
+
+ls5 = [1, {'name':"kamal", 'age':20}, (1, 2, 3, 4), {"name":"durga", "age": 20}]
+print(ls5)
